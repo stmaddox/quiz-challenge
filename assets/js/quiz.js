6d94959497b7c48ddcc7d61d3b5cdafcc01447d7
stmaddox <sean.maddox89@yahoo.com>
2022-05-30 18:23:57 -0700

    added psuedo code

---
+++ assets/js/quiz.js
@@ -0,0 +1,6 @@
+//Step 1. Have a timer that starts once the start button is clicked
+//Step 2. After I answer the question I am given another question
+//Step 3. If I answer a question incorrectly time is taken away from the clock
+//Step 4. When all questions are answered or if time reaches 0, the quiz is over
+//Step 5. I can save my initials and score once the game/quiz is over
+
