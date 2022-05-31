e32e8d10d5321473c9496c2c0c7f1563f084147c
stmaddox <sean.maddox89@yahoo.com>
2022-05-30 21:04:53 -0700

    added some page layout

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,6 +1,7 @@
-//Step 1. Have a timer that starts once the start button is clicked
-//Step 2. After I answer the question I am given another question
-//Step 3. If I answer a question incorrectly time is taken away from the clock
+//Step 1. Once start button is clicked a timer starts and the introduction page becomes hidden and the question with answers are displayed
+//Step 2. After selecting the answer button it will display/compare if the answer is correct or incorrect
+//Step 3. If I answer a question incorrectly 15 seconds is taken away from the clock, then go to the next question
 //Step 4. When all questions are answered or if time reaches 0, the quiz is over
-//Step 5. I can save my initials and score once the game/quiz is over
+//Step 5. Once the quiz is over, the remaining time will be your score. You will be able to enter your initials to save your score
+//Step 6. Create a high score board to display score. Include a "clear high scores" button and "go back" button on this screen
 
